b71fcf084f18df6c7901faebbd02c7f42e3b7584
dirksteynberg <bytemedirk@proton.me>
2024-07-24 09:36:57 +0200

    Task - Event Management

--- event_management/urls.py
+++ event_management/urls.py
@@ -1,5 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from .views import calendar, add_event, get_events, list_events, edit_event, delete_event
 
 urlpatterns = [
+    path("calendar/", calendar, name="calendar"),
+    path("add_event/", add_event, name="add_event"),
+    path("get_events/", get_events, name="get_events"),
+    path("list_events/", list_events, name="list_events"),
+    path("edit_event/<int:event_id>/", edit_event, name="edit_event"),
+    path("delete_event/<int:event_id>/", delete_event, name="delete_event"),
 ]
--- event_management/views.py
+++ event_management/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import EventForm
+from .models import Event
+
+@login_required(login_url="login")
+def calendar(request):
+    events = Event.objects.all()
+    return render(request, 'event_management/calendar.html', {'events': events})
+
+
+@login_required(login_url="login")
+def add_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)  # Save the form temporarily without committing to the database
+            event.owner = request.user  # Set the owner of the event to the current user
+            event.save()  # Now save the event to the database
+            return redirect('calendar')
+    else:
+        form = EventForm()
+    return render(request, 'event_management/add_event.html', {'form': form})
+
+
+@login_required(login_url="login")
+def list_events(request):
+    """List users created events"""
+    events = Event.objects.filter(owner=request.user)
+    return render(request, 'event_management/list_events.html', {'events': events})
+
+
+@login_required(login_url="login")
+def get_events(request):
+    events = Event.objects.all()
+    event_list = []
+    for event in events:
+        event_list.append({
+            'id': event.id,
+            'title': event.title,
+            'start': event.start_time.isoformat(),
+            'end': event.end_time.isoformat(),
+        })
+    return JsonResponse(event_list, safe=False)
+
+
+@login_required(login_url="login")
+def edit_event(request, event_id):
+    event = Event.objects.get(id=event_id)
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=event)
+        if form.is_valid():
+            form.save()
+            return redirect('calendar')
+    else:
+        form = EventForm(instance=event)
+    return render(request, 'event_management/edit_event.html', {'form': form, 'event': event})
+
+
+@login_required(login_url="login")
+def delete_event(request, event_id):
+    event = Event.objects.get(id=event_id)
+    event.delete()
+    return redirect('calendar')
