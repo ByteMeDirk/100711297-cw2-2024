277a0cfb1f86d7ba8d79f70d18e4a144c092351d
dirksteynberg <bytemedirk@proton.me>
2024-07-24 09:34:04 +0200

    Task - Task Management

---
+++ task_management/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from .models import Task
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date']
+        widgets = {
+            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Title",
+            }
+        )
+    )
+
+    description = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Description",
+            }
+        )
+    )
+
+    due_date = forms.DateTimeField(
+        widget=forms.DateTimeInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Due Date",
+                "type": "datetime-local",
+            }
+        )
+    )
+
+    owner = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        widget=forms.Select(
+            attrs={
+                "class": "form-control",
+            }
+        )
+    )
--- task_management/models.py
+++ task_management/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    completed = models.BooleanField(default=False)
+
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name='task_creator')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name='task_owner')
+
+    def __str__(self):
+        return self.title
